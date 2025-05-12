a89bbdc44ac5251d88ab2028c49c0dda6edd5283
2HEEJIN <remember2076@gmail.com>
2025-05-12 12:46:33 +0900

    [이희진] 삭제 버튼 빨간색으로 변경

--- src/presentation/components/atoms/DeleteButton.tsx
+++ src/presentation/components/atoms/DeleteButton.tsx
@@ -8,7 +8,7 @@ export function DeleteButton({ onClick }: DeleteButtonProps) {
     return (
     <button
         onClick={onClick}
-        className="bg-blue-100 text-blue-600 px-3 py-1 rounded">삭제
+        className="bg-red-100 text-red-600 px-3 py-1 rounded">삭제
     </button>
     );
 }
