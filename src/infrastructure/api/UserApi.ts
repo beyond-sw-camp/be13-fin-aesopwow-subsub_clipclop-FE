57b7dc889a7feb5faa83829657c204ddd9a2f348
blooper20 <yjw127588@gmail.com>
2025-05-30 14:32:57 +0900

    [유재우] fix: isDeleted

--- src/infrastructure/api/UserApi.ts
+++ src/infrastructure/api/UserApi.ts
@@ -3,6 +3,7 @@ import axiosInstance from "@/infrastructure/api/Axios";
 export interface UserDeleteRequest {
     reason?: string;
     password?: string;
+    isDeleted?: boolean;
 }
 
 export async function deleteUser(userNo: number, request: UserDeleteRequest): Promise<void> {
