ac370348c5a927bd198f8be2302919c26870c7db
DESKTOP-KKJ0FNN\jimin <jimin001006@naver.com>
2025-06-09 15:44:49 +0900

    [권지민] 배포 1

--- src/infrastructure/api/Axios.ts
+++ src/infrastructure/api/Axios.ts
@@ -1,20 +1,9 @@
-// import axios from 'axios';
-
-// export const axiosInstance = axios.create({
-//   // baseURL: 'https://api.dagudok-service.com/api', // 베포용 백엔드 API 주소
-//   baseURL: 'http://localhost:8001/api', // 백엔드 API 주소
-//   withCredentials: true, // CORS 쿠키 인증을 사용하는 경우
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-// });
-
-// export default axiosInstance;
-
 import axios from 'axios';
 
-const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8001/api',
+export const axiosInstance = axios.create({
+  baseURL: 'https://api.dagudok-service.com/api', // 베포용 백엔드 API 주소
+  // baseURL: 'http://localhost:8001/api', // 백엔드 API 주소
+  withCredentials: true, // CORS 쿠키 인증을 사용하는 경우
   headers: {
     'Content-Type': 'application/json',
   },
